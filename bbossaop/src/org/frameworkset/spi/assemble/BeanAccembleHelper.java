a4f46addea4e88feffb58c027ab3e409a4e6c472
yin-bp <yin-bp@163.com>
2012-01-06 11:44:46 +0800

    ok

--- bbossaop/src/org/frameworkset/spi/assemble/BeanAccembleHelper.java
+++ bbossaop/src/org/frameworkset/spi/assemble/BeanAccembleHelper.java
@@ -1,9 +1,6 @@
 package org.frameworkset.spi.assemble;
 
-import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
-import java.beans.Introspector;
-import java.beans.PropertyDescriptor;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
