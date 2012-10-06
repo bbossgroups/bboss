4fe886ca10e456fc27a7d1726814a4e92f804f6d
yin-bp <yin-bp@163.com>
2012-10-06 20:36:03 +0800

    清楚冗余文件

--- bboss-plugin-wordpdf/distrib/WebRoot/wordpdf/loadword.js
+++
@@ -1,5 +0,0 @@
-var s = ""
-s += "<OBJECT id=DSOFramer name=DSOFramer align='middle' style='LEFT: 0px; WIDTH: 100%; TOP: 0px; HEIGHT: 100%'"
-s += "classid=clsid:00460182-9E5E-11D5-B7C8-B8269041DD57 codeBase=dsoframer.ocx#version=2,2,1,2' >"
-s += "</OBJECT>"
-document.write(s) 
