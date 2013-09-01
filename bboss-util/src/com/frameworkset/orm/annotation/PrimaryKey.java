bf5c35433c8ed695473b0f3f9ebdafc543111a84
yin-bp <yin-bp@163.com>
2013-09-01 22:17:49 +0800

    遗漏的文件上传

---
+++ bboss-util/src/com/frameworkset/orm/annotation/PrimaryKey.java
@@ -0,0 +1,43 @@
+/*
+ *  Copyright 2008 biaoping.yin
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.frameworkset.orm.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PrimaryKey {
+	/**
+	 * Ö÷¼üÃû³Æ
+	 * @return
+	 */
+	String name() default "";
+	String pkname();
+	
+	boolean auto() default false;
+	
+	/**
+	 * sequence,string,int,long
+	 * @return
+	 */
+	String type() default "long";
+
+}
