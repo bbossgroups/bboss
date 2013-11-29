d6836841b43da00370f0c34ab56d94a8b00215f0
yin biaoping <yin-bp@163.com>
2013-11-29 08:45:14 +0800

    增加bboss cxf 2.7.6通过认证调用sap服务案例

---
+++ bestpractice/bboss-clientproxy/src/org/frameworkset/spi/remote/wsclient/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "urn:sap-com:document:sap:soap:functions:mc-style")
+package org.frameworkset.spi.remote.wsclient;
---
+++ bestpractice/bboss-clientproxy/src/org/frameworkset/spi/remote/wsclient1/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "org.frameworkset.web.ws")
+package org.frameworkset.spi.remote.wsclient1;
