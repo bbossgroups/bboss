78b99d97f691e4ad2b0ae40ace8d89fc98b1845d
yin-bp <yin-bp@163.com>
2014-04-10 18:35:21 +0800

    存储令牌的数据库脚本

---
+++ bboss-mvc/src/org/frameworkset/web/token/token.sql
@@ -0,0 +1,105 @@
+
+-----------------------------------------------------------------------------
+-- authtemptokens
+-----------------------------------------------------------------------------
+DROP TABLE authtemptokens CASCADE CONSTRAINTS;
+
+CREATE TABLE authtemptokens
+(
+    ID VARCHAR2(100) NOT NULL,
+    token VARCHAR2(500),
+    createTime NUMBER(20),
+    lastVistTime NUMBER(20),
+    livetime NUMBER(20),
+    appid VARCHAR2(100),
+    secret VARCHAR2(100),
+    validate_ VARCHAR2(10)
+);
+
+ALTER TABLE authtemptokens
+    ADD CONSTRAINT authtemptokens_PK
+PRIMARY KEY (ID);
+
+
+
+
+
+
+-----------------------------------------------------------------------------
+-- dualtokens
+-----------------------------------------------------------------------------
+DROP TABLE dualtokens CASCADE CONSTRAINTS;
+
+CREATE TABLE dualtokens
+(
+    ID VARCHAR2(100) NOT NULL,
+    token VARCHAR2(1000),
+    createTime NUMBER(20),
+    lastVistTime NUMBER(20),
+    livetime NUMBER(20),
+    appid VARCHAR2(100),
+    secret VARCHAR2(100),
+    validate_ VARCHAR2(10)
+);
+
+ALTER TABLE dualtokens
+    ADD CONSTRAINT dualtokens_PK
+PRIMARY KEY (ID);
+
+
+
+
+
+
+-----------------------------------------------------------------------------
+-- temptokens
+-----------------------------------------------------------------------------
+DROP TABLE temptokens CASCADE CONSTRAINTS;
+
+CREATE TABLE temptokens
+(
+    ID VARCHAR2(100) NOT NULL,
+    token VARCHAR2(1000),
+    createTime NUMBER(20),
+    livetime NUMBER(20),
+    validate_ VARCHAR2(10)
+);
+
+ALTER TABLE temptokens
+    ADD CONSTRAINT temptokens_PK
+PRIMARY KEY (ID);
+
+
+
+
+
+
+-----------------------------------------------------------------------------
+-- eckeypairs
+-----------------------------------------------------------------------------
+DROP TABLE eckeypairs CASCADE CONSTRAINTS;
+
+CREATE TABLE eckeypairs
+(
+    appid VARCHAR2(100) NOT NULL,
+    privateKey VARCHAR2(500),
+    createTime NUMBER(20),
+    publicKey VARCHAR2(500)
+);
+
+ALTER TABLE eckeypairs
+    ADD CONSTRAINT eckeypairs_PK
+PRIMARY KEY (appid);
+
+
+
+
+
+
+
+
+
+
+
+
+
