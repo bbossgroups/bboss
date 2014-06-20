353009604c73634e259a7e76b4b0adddd44fb351
yin-bp <yin-bp@163.com>
2014-06-20 17:21:40 +0800

    解决sqlserver 2008兼容信息问题

--- bbossaop/src/org/frameworkset/soa/SerialException.java
+++ bbossaop/src/org/frameworkset/soa/SerialException.java
@@ -46,10 +46,6 @@ public class SerialException extends RuntimeException {
 		// TODO Auto-generated constructor stub
 	}
 
-	public SerialException(String message, Throwable cause,
-			boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
+	
 
 }
