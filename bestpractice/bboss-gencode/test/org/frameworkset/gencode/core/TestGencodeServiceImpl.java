d635e3e1197dc4ce6fee10941caca538b64f04d6
yin-bp <yin-bp@163.com>
2015-03-09 17:26:55 +0800

    添加代码生成框架注释

--- bestpractice/bboss-gencode/test/org/frameworkset/gencode/core/TestGencodeServiceImpl.java
+++ bestpractice/bboss-gencode/test/org/frameworkset/gencode/core/TestGencodeServiceImpl.java
@@ -15,19 +15,21 @@ public class TestGencodeServiceImpl {
 		 
 		GencodeServiceImpl gencodeService = new GencodeServiceImpl();
 		ModuleMetaInfo moduleMetaInfo = new ModuleMetaInfo();
-		moduleMetaInfo.setTableName("td_app_bom");
-		moduleMetaInfo.setModuleName("appbom");
-		moduleMetaInfo.setDatasourceName("bspf");
-		moduleMetaInfo.setPackagePath("org.frameworkset.demo");
+		moduleMetaInfo.setTableName("td_app_bom");//指定表名，根据表结构来生成所有的文件
+		moduleMetaInfo.setModuleName("appbom");//指定模块名称，源码和配置文件都会存放在相应模块名称的目录下面
+		moduleMetaInfo.setDatasourceName("bspf");//指定数据源名称，在poolman.xml文件中配置
+		moduleMetaInfo.setPackagePath("org.frameworkset.demo");//java程序对应的包路径
 		moduleMetaInfo.setServiceName("AppbomManagerService");
-		moduleMetaInfo.setSourcedir("d:/sources");
-		moduleMetaInfo.setIgnoreEntityFirstToken(true);
-		moduleMetaInfo.setAuthor("yinbp");
-		moduleMetaInfo.setCompany("sany");
-		moduleMetaInfo.setVersion("v1.0");
+		moduleMetaInfo.setSourcedir("d:/sources");//生成文件存放的物理目录，如果不存在，会自动创建
+		moduleMetaInfo.setIgnoreEntityFirstToken(true); //忽略表的第一个下滑线签名的token，例如表名td_app_bom中，只会保留app_bom部分，然后根据这部分来生成实体、配置文件名称
+		moduleMetaInfo.setAuthor("yinbp");//程序作者
+		moduleMetaInfo.setCompany("sany");//公司信息
+		moduleMetaInfo.setVersion("v1.0");//版本信息
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		String date = format.format(new Date());
-		moduleMetaInfo.setDate(date);
+		moduleMetaInfo.setDate(date);//指定日期
+		
+		/************以下代码片段指定界面查询字段，以及查询条件组合方式、是否是模糊查询等*******/
 		ConditionField bm = new ConditionField();
 		
 		bm.setFieldName("bm");
@@ -42,7 +44,9 @@ public class TestGencodeServiceImpl {
 		softLevel.setFieldName("softLevel");
 	
 		gencodeService.addCondition(softLevel);
+		/************以上代码片段指定界面查询字段，以及查询条件组合方式、是否是模糊查询等********/
 		
+		/************以下代码片段指定界面排序字段**********************************/
 		SortField id = new SortField();
 		id.setColumnName("ID");
 		id.setFieldName("id");
@@ -53,7 +57,9 @@ public class TestGencodeServiceImpl {
 		sbm.setFieldName("bm");
 		sbm.setDesc(false);
 		gencodeService.addSortField(sbm);
-		gencodeService.genCode(moduleMetaInfo);
+		/************以上代码片段指定界面排序字段**********************************/
+		
+		gencodeService.genCode(moduleMetaInfo);//执行代码生成逻辑
 	}
 
 }
