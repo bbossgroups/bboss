188990a2fb2dc9d1df508b8227ccb43a1a9c7874
yin-bp <yin-bp@163.com>
2015-12-23 10:21:31 +0800

    将bboss-security和bboss-security-web两个子工程提升为一级工程，将会话共享项目从bboss项目中剥离，提升为一级项目

--- bboss-security/src/org/frameworkset/security/session/SessionListener.java
+++
@@ -1,9 +0,0 @@
-package org.frameworkset.security.session;
-
-public interface SessionListener {
-	public void createSession(SessionEvent event);
-	public void destroySession(SessionEvent event);
-	public void addAttribute(SessionEvent event);
-	public void removeAttribute(SessionEvent event);
-
-}
