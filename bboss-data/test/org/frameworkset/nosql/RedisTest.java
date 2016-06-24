800e2539c617fba9c0baeeb4b213fdb1e498400e
yin-bp <yin-bp@163.com>
2016-06-24 20:47:45 +0800

    发布bboss-rt.jar未设置好Main-class

--- bboss-data/test/org/frameworkset/nosql/RedisTest.java
+++ bboss-data/test/org/frameworkset/nosql/RedisTest.java
@@ -19,11 +19,22 @@ public class RedisTest {
 			redisHelper.set("test", "value1");
 			String value = redisHelper.get("test");
 			System.out.println("test="+value);
-			redisHelper.set("foo", "fasdfasf");
+			redisHelper.setex("foo", 1,"fasdfasf");//指定缓存有效期1秒
 			
-			value = redisHelper.get("foo");
+			System.out.println("foo ttl="+redisHelper.ttl("foo"));//获取有效期
+			value = redisHelper.get("foo");//获取数据
 			System.out.println("foo="+value);
+			//删除数据
+			redisHelper.del("foo");
+			value = redisHelper.getSet("fowwero","test");
+			
+			System.out.println("fowwero="+value);
+			value = redisHelper.getSet("fowwero","eeee");//获取后修改数据
+			
+			System.out.println("fowwero="+value);
+			
 			value = redisHelper.get("fowwero");
+			
 			System.out.println("fowwero="+value);
 		}
 		finally
