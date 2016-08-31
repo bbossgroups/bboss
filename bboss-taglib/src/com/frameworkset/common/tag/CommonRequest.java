3059399eeca07608c89cacb5a1251d26bf65d3ea
yin-bp <yin-bp@163.com>
2016-08-31 23:06:07 +0800

    更新gradle引用关系

--- bboss-taglib/src/com/frameworkset/common/tag/CommonRequest.java
+++ bboss-taglib/src/com/frameworkset/common/tag/CommonRequest.java
@@ -36,15 +36,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.Principal;
+import java.util.Collection;
 import java.util.Enumeration;
 import java.util.Locale;
 import java.util.Map;
 
+import javax.servlet.AsyncContext;
+import javax.servlet.DispatcherType;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 
 import org.apache.log4j.Logger;
 
@@ -670,14 +679,85 @@ public class CommonRequest implements HttpServletRequest {
 	public String getLocalName() {
 		// TODO Auto-generated method stub
 		//return request.getLocalName();
-		return null;
+		return request.getLocalName();
 	}
 
     public String getMobileEncode() {
         return mobileEncode;
     }
 
-    private void jbInit() throws Exception {
-    }
+
+	@Override
+	public ServletContext getServletContext() {
+		// TODO Auto-generated method stub
+		return request.getServletContext();
+	}
+
+	@Override
+	public AsyncContext startAsync() throws IllegalStateException {
+		// TODO Auto-generated method stub
+		return request.startAsync();
+	}
+
+	@Override
+	public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse)
+			throws IllegalStateException {
+		// TODO Auto-generated method stub
+		return request.startAsync(servletRequest, servletResponse);
+	}
+
+	@Override
+	public boolean isAsyncStarted() {
+		// TODO Auto-generated method stub
+		return request.isAsyncStarted();
+	}
+
+	@Override
+	public boolean isAsyncSupported() {
+		// TODO Auto-generated method stub
+		return request.isAsyncSupported();
+	}
+
+	@Override
+	public AsyncContext getAsyncContext() {
+		// TODO Auto-generated method stub
+		return request.getAsyncContext();
+	}
+
+	@Override
+	public DispatcherType getDispatcherType() {
+		// TODO Auto-generated method stub
+		return request.getDispatcherType();
+	}
+
+	@Override
+	public boolean authenticate(HttpServletResponse response) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		return request.authenticate(response);
+	}
+
+	@Override
+	public void login(String username, String password) throws ServletException {
+		// TODO Auto-generated method stub
+		request.login(username, password);
+	}
+
+	@Override
+	public void logout() throws ServletException {
+		// TODO Auto-generated method stub
+		request.logout();
+	}
+
+	@Override
+	public Collection<Part> getParts() throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		return request.getParts();
+	}
+
+	@Override
+	public Part getPart(String name) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		return request.getPart(name);
+	}
 
 }
