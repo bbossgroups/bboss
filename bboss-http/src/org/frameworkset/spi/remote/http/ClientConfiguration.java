b586d47123695a6ec3691bbd53dc6c78b4aaa436
yinbp <yin-bp@163.com>
2016-11-20 16:29:29 +0800

    完善http组件

---
+++ bboss-http/src/org/frameworkset/spi/remote/http/ClientConfiguration.java
@@ -0,0 +1,282 @@
+/**
+ * 
+ */
+package org.frameworkset.spi.remote.http;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.charset.CodingErrorAction;
+import java.util.Arrays;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.Consts;
+import org.apache.http.Header;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.AuthSchemes;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.ConnectionConfig;
+import org.apache.http.config.MessageConstraints;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.config.SocketConfig;
+import org.apache.http.conn.DnsResolver;
+import org.apache.http.conn.HttpConnectionFactory;
+import org.apache.http.conn.ManagedHttpClientConnection;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.DefaultHttpResponseFactory;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.DefaultHttpResponseParser;
+import org.apache.http.impl.conn.DefaultHttpResponseParserFactory;
+import org.apache.http.impl.conn.ManagedHttpClientConnectionFactory;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.impl.conn.SystemDefaultDnsResolver;
+import org.apache.http.impl.io.DefaultHttpRequestWriterFactory;
+import org.apache.http.io.HttpMessageParser;
+import org.apache.http.io.HttpMessageParserFactory;
+import org.apache.http.io.HttpMessageWriterFactory;
+import org.apache.http.io.SessionInputBuffer;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicLineParser;
+import org.apache.http.message.LineParser;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.util.CharArrayBuffer;
+
+/**
+ * This example demonstrates how to customize and configure the most common aspects
+ * of HTTP request execution and connection management.
+ */
+/**
+ * @author yinbp
+ *
+ * @Date:2016-11-20 11:50:36
+ */
+public class ClientConfiguration {
+	private final static int TIMEOUT_CONNECTION = 20000;
+	private final static int TIMEOUT_SOCKET = 20000;
+	private final static int RETRY_TIME = 3;
+	private static HttpClient httpclient;
+	private static RequestConfig defaultRequestConfig ;
+	public static final ContentType TEXT_PLAIN_UTF_8 = ContentType.create(
+            "text/plain", Consts.UTF_8);
+	/**
+	 * 
+	 */
+	public ClientConfiguration() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public final static HttpClient getHttpClient()  throws Exception {
+		if(httpclient != null)
+			return httpclient;
+		synchronized(ClientConfiguration.class)
+		{
+			if(httpclient != null)
+				return httpclient;
+	        // Use custom message parser / writer to customize the way HTTP
+	        // messages are parsed from and written out to the data stream.
+	        HttpMessageParserFactory<HttpResponse> responseParserFactory = new DefaultHttpResponseParserFactory() {
+	
+	            @Override
+	            public HttpMessageParser<HttpResponse> create(
+	                SessionInputBuffer buffer, MessageConstraints constraints) {
+	                LineParser lineParser = new BasicLineParser() {
+	
+	                    @Override
+	                    public Header parseHeader(final CharArrayBuffer buffer) {
+	                        try {
+	                            return super.parseHeader(buffer);
+	                        } catch (ParseException ex) {
+	                            return new BasicHeader(buffer.toString(), null);
+	                        }
+	                    }
+	
+	                };
+	                return new DefaultHttpResponseParser(
+	                    buffer, lineParser, DefaultHttpResponseFactory.INSTANCE, constraints) {
+	
+	                    @Override
+	                    protected boolean reject(final CharArrayBuffer line, int count) {
+	                        // try to ignore all garbage preceding a status line infinitely
+	                        return false;
+	                    }
+	
+	                };
+	            }
+	
+	        };
+	        HttpMessageWriterFactory<HttpRequest> requestWriterFactory = new DefaultHttpRequestWriterFactory();
+	
+	        // Use a custom connection factory to customize the process of
+	        // initialization of outgoing HTTP connections. Beside standard connection
+	        // configuration parameters HTTP connection factory can define message
+	        // parser / writer routines to be employed by individual connections.
+	        HttpConnectionFactory<HttpRoute, ManagedHttpClientConnection> connFactory = new ManagedHttpClientConnectionFactory(
+	                requestWriterFactory, responseParserFactory);
+	
+	        // Client HTTP connection objects when fully initialized can be bound to
+	        // an arbitrary network socket. The process of network socket initialization,
+	        // its connection to a remote address and binding to a local one is controlled
+	        // by a connection socket factory.
+	
+	        // SSL context for secure connections can be created either based on
+	        // system or application specific properties.
+	        SSLContext sslcontext = SSLContexts.createSystemDefault();
+	
+	        // Create a registry of custom connection socket factories for supported
+	        // protocol schemes.
+	        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+	            .register("http", PlainConnectionSocketFactory.INSTANCE)
+	            .register("https", new SSLConnectionSocketFactory(sslcontext))
+	            .build();
+	
+	        // Use custom DNS resolver to override the system DNS resolution.
+	        DnsResolver dnsResolver = new SystemDefaultDnsResolver() {
+	
+	            @Override
+	            public InetAddress[] resolve(final String host) throws UnknownHostException {
+	                if (host.equalsIgnoreCase("localhost")) {
+	                    return new InetAddress[] { InetAddress.getByAddress(new byte[] {127, 0, 0, 1}) };
+	                } else {
+	                    return super.resolve(host);
+	                }
+	            }
+	
+	        };
+	
+	        // Create a connection manager with custom configuration.
+	        PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager(
+	                socketFactoryRegistry, connFactory, dnsResolver);
+	
+	        // Create socket configuration
+	        SocketConfig socketConfig = SocketConfig.custom()
+	            .setTcpNoDelay(true)
+	            .setSoTimeout(TIMEOUT_SOCKET)
+	            .build();
+	        // Configure the connection manager to use socket configuration either
+	        // by default or for a specific host.
+	        connManager.setDefaultSocketConfig(socketConfig);
+	        connManager.setSocketConfig(new HttpHost("localhost", 80), socketConfig);
+	        // Validate connections after 1 sec of inactivity
+	        connManager.setValidateAfterInactivity(1000);
+	        
+	        
+	        // Create message constraints
+	        MessageConstraints messageConstraints = MessageConstraints.custom()
+	            .setMaxHeaderCount(200)
+	            .setMaxLineLength(2000)
+	            .build();
+	        // Create connection configuration
+	        ConnectionConfig connectionConfig = ConnectionConfig.custom()
+	            .setMalformedInputAction(CodingErrorAction.IGNORE)
+	            .setUnmappableInputAction(CodingErrorAction.IGNORE)
+	            .setCharset(Consts.UTF_8)
+	            .setMessageConstraints(messageConstraints)
+	            .build();
+	        // Configure the connection manager to use connection configuration either
+	        // by default or for a specific host.
+	        connManager.setDefaultConnectionConfig(connectionConfig);
+	        connManager.setConnectionConfig(new HttpHost("localhost", 80), ConnectionConfig.DEFAULT);
+	
+	        // Configure total max or per route limits for persistent connections
+	        // that can be kept in the pool or leased by the connection manager.
+	        connManager.setMaxTotal(100);
+	        connManager.setDefaultMaxPerRoute(10);
+	        connManager.setMaxPerRoute(new HttpRoute(new HttpHost("localhost", 80)), 20);
+	
+	        // Use custom cookie store if necessary.
+	        CookieStore cookieStore = new BasicCookieStore();
+	        
+	        // Use custom credentials provider if necessary.
+	        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+	        // Create global request configuration
+	        defaultRequestConfig = RequestConfig.custom()
+	            .setCookieSpec(CookieSpecs.DEFAULT)
+	            .setExpectContinueEnabled(true)
+	            .setTargetPreferredAuthSchemes(Arrays.asList(AuthSchemes.NTLM, AuthSchemes.DIGEST))
+	            .setProxyPreferredAuthSchemes(Arrays.asList(AuthSchemes.BASIC))
+	            .build();
+	
+	        // Create an HttpClient with the given custom dependencies and configuration.
+	        httpclient = HttpClients.custom()
+	            .setConnectionManager(connManager)
+	            .setDefaultCookieStore(cookieStore)
+	            .setDefaultCredentialsProvider(credentialsProvider)
+	            //.setProxy(new HttpHost("myproxy", 8080))
+	            .setDefaultRequestConfig(defaultRequestConfig)
+	            .build();
+		}
+        return httpclient;
+
+//        try {
+//            HttpGet httpget = new HttpGet("http://www.baidu.com");
+//            // Request configuration can be overridden at the request level.
+//            // They will take precedence over the one set at the client level.
+//            RequestConfig requestConfig = RequestConfig.copy(defaultRequestConfig)
+//                .setSocketTimeout(5000)
+//                .setConnectTimeout(5000)
+//                .setConnectionRequestTimeout(5000)
+//               // .setProxy(new HttpHost("myotherproxy", 8080))
+//                .build();
+//            httpget.setConfig(requestConfig);
+//
+//            // Execution context can be customized locally.
+//            HttpClientContext context = HttpClientContext.create();
+//            // Contextual attributes set the local context level will take
+//            // precedence over those set at the client level.
+//            context.setCookieStore(cookieStore);
+//            context.setCredentialsProvider(credentialsProvider);
+//
+//            System.out.println("executing request " + httpget.getURI());
+//            CloseableHttpResponse response = httpclient.execute(httpget, context);
+//            try {
+//                System.out.println("----------------------------------------");
+//                System.out.println(response.getStatusLine());
+//                System.out.println(EntityUtils.toString(response.getEntity()));
+//                System.out.println("----------------------------------------");
+//
+//                // Once the request has been executed the local context can
+//                // be used to examine updated state and various objects affected
+//                // by the request execution.
+//
+//                // Last executed request
+//                context.getRequest();
+//                // Execution route
+//                context.getHttpRoute();
+//                // Target auth state
+//                context.getTargetAuthState();
+//                // Proxy auth state
+//                context.getTargetAuthState();
+//                // Cookie origin
+//                context.getCookieOrigin();
+//                // Cookie spec used
+//                context.getCookieSpec();
+//                // User security token
+//                context.getUserToken();
+//
+//            } finally {
+//                response.close();
+//            }
+//        } finally {
+//            httpclient.close();
+//        }
+    }
+
+	public static RequestConfig getDefaultRequestConfig() {
+		return defaultRequestConfig;
+	}
+
+}
---
+++ bboss-http/src/org/frameworkset/spi/remote/http/HttpRequestUtil.java
@@ -0,0 +1,392 @@
+/**
+ * 
+ */
+package org.frameworkset.spi.remote.http;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.http.Consts;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * @author yinbp
+ *
+ * @Date:2016-11-20 11:39:59
+ */
+public class HttpRequestUtil {
+
+	public static final String UTF_8 = "UTF-8";
+	// public static final String DESC = "descend";
+	// public static final String ASC = "ascend";
+
+	private final static int TIMEOUT_CONNECTION = 20000;
+	private final static int TIMEOUT_SOCKET = 20000;
+	private final static int RETRY_TIME = 3;
+
+	private static HttpClient getHttpClient() throws Exception {
+		return ClientConfiguration.getHttpClient();
+	}
+
+	private static String getCookie() {
+		// if(appCookie == null || appCookie == "") {
+		// appCookie = appContext.getProperty("cookie");
+		// }
+		// return appCookie;
+		return null;
+	}
+
+	private static String getUserAgent() {
+		// if(appUserAgent == null || appUserAgent == "") {
+		// StringBuilder ua = new StringBuilder("OSChina.NET");
+		// ua.append('/'+appContext.getPackageInfo().versionName+'_'+appContext.getPackageInfo().versionCode);//App版本
+		// ua.append("/Android");//手机系统平台
+		// ua.append("/"+android.os.Build.VERSION.RELEASE);//手机系统版本
+		// ua.append("/"+android.os.Build.MODEL); //手机型号
+		// ua.append("/"+appContext.getAppId());//客户端唯一标识
+		// appUserAgent = ua.toString();
+		// }
+		// return appUserAgent;
+		return null;
+	}
+
+	private static HttpGet getHttpGet(String url, String cookie, String userAgent) {
+
+		HttpGet httpget = new HttpGet(url);
+		// Request configuration can be overridden at the request level.
+		// They will take precedence over the one set at the client level.
+		RequestConfig requestConfig = RequestConfig.copy(ClientConfiguration.getDefaultRequestConfig())
+				.setSocketTimeout(5000).setConnectTimeout(TIMEOUT_CONNECTION)
+				.setConnectionRequestTimeout(TIMEOUT_CONNECTION)
+				// .setProxy(new HttpHost("myotherproxy", 8080))
+				.build();
+		httpget.setConfig(requestConfig);
+		httpget.addHeader("Host", "www.bbossgroups.com");
+		httpget.addHeader("Connection", "Keep-Alive");
+		if (cookie != null)
+			httpget.addHeader("Cookie", cookie);
+		if (userAgent != null)
+			httpget.addHeader("User-Agent", userAgent);
+		return httpget;
+	}
+
+	private static HttpPost getHttpPost(String url, String cookie, String userAgent) {
+		HttpPost httpPost = new HttpPost(url);
+		RequestConfig requestConfig = RequestConfig.copy(ClientConfiguration.getDefaultRequestConfig())
+				.setSocketTimeout(5000).setConnectTimeout(TIMEOUT_CONNECTION)
+				.setConnectionRequestTimeout(TIMEOUT_CONNECTION)
+				// .setProxy(new HttpHost("myotherproxy", 8080))
+				.build();
+		httpPost.setConfig(requestConfig);
+		httpPost.addHeader("Host", "www.bbossgroups.com");
+		httpPost.addHeader("Connection", "Keep-Alive");
+		if (cookie != null)
+			httpPost.addHeader("Cookie", cookie);
+		if (userAgent != null)
+			httpPost.addHeader("User-Agent", userAgent);
+
+		return httpPost;
+	}
+
+	public static String httpGetforString(String url) throws Exception {
+		return httpGetforString(url, (String) null, (String) null);
+	}
+
+	/**
+	 * get请求URL
+	 * 
+	 * @param url
+	 * @throws AppException
+	 */
+	public static String httpGetforString(String url, String cookie, String userAgent) throws Exception {
+		// String cookie = getCookie();
+		// String userAgent = getUserAgent();
+
+		HttpClient httpClient = null;
+		HttpGet httpGet = null;
+
+		String responseBody = "";
+		int time = 0;
+		do {
+			try {
+				httpClient = getHttpClient();
+				httpGet = getHttpGet(url, cookie, userAgent);
+
+				// Create a custom response handler
+				ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+					@Override
+					public String handleResponse(final HttpResponse response)
+							throws ClientProtocolException, IOException {
+						int status = response.getStatusLine().getStatusCode();
+						if (status >= 200 && status < 300) {
+							HttpEntity entity = response.getEntity();
+							return entity != null ? EntityUtils.toString(entity) : null;
+						} else {
+							throw new ClientProtocolException("Unexpected response status: " + status);
+						}
+					}
+
+				};
+				responseBody = httpClient.execute(httpGet, responseHandler);
+				break;
+			} catch (ClientProtocolException e) {
+				throw new HttpRuntimeException("请求异常：", e);
+			} catch (IOException e) {
+				time++;
+				if (time < RETRY_TIME) {
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e1) {
+					}
+					continue;
+				}
+				// 发生致命的异常，可能是协议不对或者返回的内容有问题
+				throw new HttpRuntimeException("请求异常：", e);
+			} catch (Exception e) {
+				time++;
+				if (time < RETRY_TIME) {
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e1) {
+					}
+					continue;
+				}
+				// 发生网络异常
+				e.printStackTrace();
+				throw new HttpRuntimeException("请求异常：", e);
+			} finally {
+				// 释放连接
+				httpGet.releaseConnection();
+				httpClient = null;
+			}
+		} while (time < RETRY_TIME);
+
+		return responseBody;
+		// //responseBody = responseBody.replaceAll("\\p{Cntrl}", "\r\n");
+		// if(responseBody.contains("result") &&
+		// responseBody.contains("errorCode") &&
+		// appContext.containsProperty("user.uid")){
+		// try {
+		// Result res = Result.parse(new
+		// ByteArrayInputStream(responseBody.getBytes()));
+		// if(res.getErrorCode() == 0){
+		// appContext.Logout();
+		// appContext.getUnLoginHandler().sendEmptyMessage(1);
+		// }
+		// } catch (Exception e) {
+		// e.printStackTrace();
+		// }
+		// }
+		// return new ByteArrayInputStream(responseBody.getBytes());
+	}
+
+	/**
+	 * 公用post方法
+	 * 
+	 * @param url
+	 * @param params
+	 * @param files
+	 * @throws AppException
+	 */
+	public static String httpPostforString(String url, Map<String, Object> params, Map<String, File> files)
+			throws Exception {
+		return httpPostforString(url, (String) null, (String) null, params, files);
+	}
+
+	/**
+	 * 公用post方法
+	 * 
+	 * @param url
+	 * @param params
+	 * @param files
+	 * @throws AppException
+	 */
+	public static String httpPostforString(String url, Map<String, Object> params) throws Exception {
+		return httpPostforString(url, (String) null, (String) null, params, (Map<String, File>) null);
+	}
+
+	/**
+	 * 公用post方法
+	 * 
+	 * @param url
+	 * @param params
+	 * @param files
+	 * @throws AppException
+	 */
+	public static String httpPostforString(String url) throws Exception {
+		return httpPostforString(url, (String) null, (String) null, (Map<String, Object>) null,
+				(Map<String, File>) null);
+	}
+	public static String httpPostforString(String url, String cookie, String userAgent,
+			Map<String, File> files) throws Exception {
+		return httpPostforString(url, cookie, userAgent, null,
+				files);
+	}
+	/**
+	 * 公用post方法
+	 * 
+	 * @param url
+	 * @param params
+	 * @param files
+	 * @throws AppException
+	 */
+	public static String httpPostforString(String url, String cookie, String userAgent, Map<String, Object> params,
+			Map<String, File> files) throws Exception {
+		// System.out.println("post_url==> "+url);
+		// String cookie = getCookie(appContext);
+		// String userAgent = getUserAgent(appContext);
+
+		HttpClient httpClient = null;
+		HttpPost httpPost = null;
+		MultipartEntityBuilder multipartEntityBuilder = MultipartEntityBuilder.create();
+//				
+//                .addPart("bin", bin)
+//                .addPart("comment", comment)
+//                .build();
+//				 FileBody bin = new FileBody(new File(args[0]));
+//        StringBody comment = new StringBody("A binary file of some kind", ContentType.TEXT_PLAIN);
+		HttpEntity httpEntity = null;
+		List<NameValuePair> paramPair = null;
+		if (files != null) {
+			// post表单参数处理
+			int length = (params == null ? 0 : params.size()) + (files == null ? 0 : files.size());
+			 
+			int i = 0;
+			boolean hasdata = false;
+			 
+			if (params != null) {
+				Iterator<Entry<String, Object>> it = params.entrySet().iterator();
+				while (it.hasNext()) {
+					Entry<String, Object> entry = it.next();
+					multipartEntityBuilder.addTextBody(entry.getKey(), String.valueOf(entry.getValue()),ClientConfiguration.TEXT_PLAIN_UTF_8);
+					hasdata = true;
+				}
+			}
+			if (files != null) {
+				Iterator<Entry<String, File>> it = files.entrySet().iterator();
+				while (it.hasNext()) {
+					Entry<String, File> entry = it.next();
+					 
+//						parts[i++] = new FilePart(entry.getKey(), entry.getValue());
+						File f = new File(String.valueOf(entry.getValue()));
+						if(f.exists())
+						{
+							 FileBody file = new FileBody(f);
+							multipartEntityBuilder.addPart(entry.getKey(), file);
+							hasdata = true;
+						}
+						else
+						{
+							
+						}
+					 
+					// System.out.println("post_key_file==> "+file);
+				}
+			}
+			if(hasdata)
+				httpEntity = multipartEntityBuilder.build();
+		} else if (params != null && params.size() > 0) {
+			paramPair = new ArrayList<NameValuePair>();
+			Iterator<Entry<String, Object>> it = params.entrySet().iterator();
+			NameValuePair paramPair_ = null;
+			for (int i = 0; it.hasNext(); i++) {
+				Entry<String, Object> entry = it.next();
+				paramPair_ = new BasicNameValuePair(entry.getKey(),String.valueOf(entry.getValue()));			 
+				paramPair.add(paramPair_);
+			}
+		}
+
+		String responseBody = "";
+		int time = 0;
+		do {
+			try {
+				httpClient = getHttpClient();
+				httpPost = getHttpPost(url, cookie, userAgent);
+			
+
+
+				
+				if (httpEntity != null ) {
+					httpPost.setEntity(httpEntity);
+				} else if(paramPair != null && paramPair.size() > 0){
+					UrlEncodedFormEntity entity = new UrlEncodedFormEntity(paramPair, Consts.UTF_8);
+
+					httpPost.setEntity(entity);
+
+				}
+				// Create a custom response handler
+				ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+					@Override
+					public String handleResponse(final HttpResponse response)
+							throws ClientProtocolException, IOException {
+						int status = response.getStatusLine().getStatusCode();
+						if (status >= 200 && status < 300) {
+							HttpEntity entity = response.getEntity();
+							return entity != null ? EntityUtils.toString(entity) : null;
+						} else {
+							throw new ClientProtocolException("Unexpected response status: " + status);
+						}
+					}
+
+				};
+				responseBody = httpClient.execute(httpPost, responseHandler);				
+				break;
+			}
+			catch(ClientProtocolException e)
+			{
+				throw new HttpRuntimeException("请求异常：", e);
+			}
+			catch (HttpException e) {
+				time++;
+				if (time < RETRY_TIME) {
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e1) {
+					}
+					continue;
+				}
+				// 发生致命的异常，可能是协议不对或者返回的内容有问题
+				throw new HttpRuntimeException("请求异常：", e);
+			} catch (IOException e) {
+				time++;
+				if (time < RETRY_TIME) {
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e1) {
+					}
+					continue;
+				}
+				// 发生网络异常
+				throw new HttpRuntimeException("请求异常：", e);
+			} finally {
+				// 释放连接
+				//httpPost.releaseConnection();
+				httpClient = null;
+			}
+		} while (time < RETRY_TIME);
+		return responseBody;
+		
+	}
+
+}
