5f2fa990243cda816437dc1f7c78542e0a2a7532
yinbp <yin-bp@163.com>
2017-12-18 15:16:27 +0800

    http组件增加对ssl的支持，具体使用方法参考文档
    http组件增加对ssl的支持，具体使用方法参考文档
    https://my.oschina.net/bboss/blog/1591463

--- bboss-http/test/org/frameworkset/http/client/TestHttpRequestbody.java
+++ bboss-http/test/org/frameworkset/http/client/TestHttpRequestbody.java
@@ -14,6 +14,17 @@ public class TestHttpRequestbody {
 
     @Test
     public void testHttpStringRequestbody() throws Exception {
-        System.out.println(HttpRequestUtil.sendStringBody("{\"id\":\"15284b36-3404-4bf8-8f14-c2114f2d97fb\",\"data\":\"国产j2ee框架 bboss\"}","http://localhost:9096/xmlrequest/xml/echohttpstring.page"));
+        System.out.println(HttpRequestUtil.sendStringBody("{\"id\":\"15284b36-3404-4bf8-8f14-c2114f2d97fb\",\"data\":\"国产j2ee框架 bboss\"}","http://localhost:8080/xmlrequest/xml/echohttpstring.page"));
+    }
+    
+    
+    @Test
+    public void testHttpsJsonRequestbody() throws Exception {
+        System.out.println(HttpRequestUtil.sendJsonBody("{\"id\":\"15284b36-3404-4bf8-8f14-c2114f2d97fb\",\"data\":\"国产j2ee框架 bboss\"}","https://bboss:8443/xmlrequest/xml/echohttpjson.page"));
+    }
+
+    @Test
+    public void testHttpsStringRequestbody() throws Exception {
+        System.out.println(HttpRequestUtil.sendStringBody("{\"id\":\"15284b36-3404-4bf8-8f14-c2114f2d97fb\",\"data\":\"国产j2ee框架 bboss\"}","https://localhost:8443/xmlrequest/xml/echohttpstring.page"));
     }
 }
