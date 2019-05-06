7d4f7b79e90e276ddde5fc2975920eabd735ee46
yinbp <yin-bp@163.com>
2019-05-06 22:49:14 +0800

    增加ESIndex注解，用于动态生成索引名称和type

--- bboss-util/src/com/frameworkset/orm/annotation/ESIndexWrapper.java
+++ bboss-util/src/com/frameworkset/orm/annotation/ESIndexWrapper.java
@@ -15,14 +15,17 @@ package com.frameworkset.orm.annotation;
  * limitations under the License.
  */
 
-import org.frameworkset.util.ClassUtil;
 import org.frameworkset.util.DataFormatUtil;
 import org.frameworkset.util.annotations.DateFormateMeta;
 import org.frameworkset.util.tokenizer.TextGrammarParser;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>Description: </p>
@@ -33,6 +36,10 @@ import java.util.List;
  * @version 1.0
  */
 public class ESIndexWrapper {
+	public static interface GetVariableValue{
+		public Object getValue(String property);
+	}
+
 	public NameInfo getNameInfo() {
 		return nameInfo;
 	}
@@ -50,16 +57,87 @@ public class ESIndexWrapper {
 		this.esIndex = esIndex;
 	}
 
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 	public static class NameInfo{
 		/**
 		 * 如果是固定的索引名称，则对应的索引名称为name字段对应的值，
 		 * 否则索引名称动态从tokens中计算得到一个动态的索引名称
 		 */
-		private String name;
+		protected String name;
 
 		private List<NameGrammarToken> tokens;
-		private void buildName(StringBuilder builder,ClassUtil.ClassInfo classInfo, Object bean){
+		private void buildName(Writer writer,GetVariableValue getVariableValue) throws IOException {
+			if(name != null){
+				writer.write(name);
+				return;
+			}
+			if(tokens == null || tokens.size() == 0){
+				return;
+			}
+			NameGrammarToken nameGrammarToken = null;
+			for(int i = 0; i < tokens.size(); i ++){
+				nameGrammarToken = tokens.get(i);
+				if(!nameGrammarToken.varibletoken()) {
+					writer.write(nameGrammarToken.getText());
+				}
+				else{
+					if(nameGrammarToken.getFieldName() != null) {
 
+//						Object va = classInfo.getPropertyValue(bean, nameGrammarToken.getFieldName());
+						Object va = getVariableValue.getValue(nameGrammarToken.getFieldName());
+						if (va == null)
+							throw new NameParserException(new StringBuilder()
+									.append(this.toString())
+									.append(",property[")
+									.append(nameGrammarToken.getFieldName()).append("] is null.").toString());
+						if (nameGrammarToken.dateformat != null) {
+							DateFormat dateFormat = DataFormatUtil.getSimpleDateFormat(nameGrammarToken.dateformat);
+							if (va instanceof Date) {
+								writer.write(dateFormat.format((Date) va));
+							} else if (va instanceof Long) {
+								writer.write(dateFormat.format(new Date((Long) va)));
+
+							} else {
+								writer.write(String.valueOf(va));
+							}
+						} else {
+							writer.write(String.valueOf(va));
+						}
+					}
+					else{ //取当前时间作为索引名称
+						DateFormat dateFormat = DataFormatUtil.getSimpleDateFormat(nameGrammarToken.dateformat);
+						Date date = new Date();
+						writer.write(dateFormat.format(date));
+
+					}
+				}
+			}
+		}
+
+
+		private void buildName(StringBuilder builder,GetVariableValue getVariableValue){
+			if(name != null){
+				builder.append(name);
+				return;
+			}
+			if(tokens == null || tokens.size() == 0){
+				return;
+			}
 			NameGrammarToken nameGrammarToken = null;
 			for(int i = 0; i < tokens.size(); i ++){
 				nameGrammarToken = tokens.get(i);
@@ -67,34 +145,45 @@ public class ESIndexWrapper {
 					builder.append(nameGrammarToken.getText());
 				}
 				else{
-					Object va = classInfo.getPropertyValue(bean,nameGrammarToken.getFieldName());
-					if(va == null)
-						throw new NameParserException(new StringBuilder()
-								.append(this.toString())
-								.append(",property[")
-								.append(nameGrammarToken.getFieldName()).append("] is null.").toString());
-					if(nameGrammarToken.dateformat != null){
-						DateFormat dateFormat = DataFormatUtil.getSimpleDateFormat(nameGrammarToken.dateformat);
-						if(va instanceof Date) {
-							builder.append(dateFormat.format((Date) va));
-						}
-						else if(va instanceof Long ){
-							builder.append(dateFormat.format(new Date((Long)  va)));
+					if(nameGrammarToken.getFieldName() != null) {
 
-						}
-						else{
+//						Object va = classInfo.getPropertyValue(bean, nameGrammarToken.getFieldName());
+						Object va = getVariableValue.getValue(nameGrammarToken.getFieldName());
+						if (va == null)
+							throw new NameParserException(new StringBuilder()
+									.append(this.toString())
+									.append(",property[")
+									.append(nameGrammarToken.getFieldName()).append("] is null.").toString());
+						if (nameGrammarToken.dateformat != null) {
+							DateFormat dateFormat = DataFormatUtil.getSimpleDateFormat(nameGrammarToken.dateformat);
+							if (va instanceof Date) {
+								builder.append(dateFormat.format((Date) va));
+							} else if (va instanceof Long) {
+								builder.append(dateFormat.format(new Date((Long) va)));
+
+							} else {
+								builder.append(va);
+							}
+						} else {
 							builder.append(va);
 						}
 					}
-					else{
-						builder.append(va);
+					else{ //取当前时间作为索引名称
+						DateFormat dateFormat = DataFormatUtil.getSimpleDateFormat(nameGrammarToken.dateformat);
+						Date date = new Date();
+						builder.append(dateFormat.format(date));
+
 					}
 				}
 			}
-
-
 		}
-		public String buildName(ClassUtil.ClassInfo classInfo, Object bean){
+
+		/**
+		 * ClassUtil.ClassInfo classInfo, Object bean
+
+		 * @return
+		 */
+		public String buildName(GetVariableValue getVariableValue){
 			if(name == null || name.equals("")){
 				if(tokens == null  || tokens.size() == 0 )
 					return null;
@@ -103,7 +192,7 @@ public class ESIndexWrapper {
 				return name;
 			}
 			StringBuilder builder = new StringBuilder();
-			buildName(builder,classInfo,bean);
+			buildName(builder,  getVariableValue);
 			return builder.toString();
 		}
 
@@ -116,8 +205,53 @@ public class ESIndexWrapper {
 		 */
 		private String type;
 		private List<NameGrammarToken> tokens;
-		private void buildType(StringBuilder builder,ClassUtil.ClassInfo classInfo, Object bean){
+		/**
+		 * ClassUtil.ClassInfo classInfo, Object bean
+		 * @param writer
+		 * @param getVariableValue
+		 */
+		private void buildType(Writer writer,GetVariableValue getVariableValue) throws IOException {
+			if(type != null){
+				writer.write(type);
+				return;
+			}
+			if(tokens == null || tokens.size() == 0){
+				return;
+			}
+			NameGrammarToken nameGrammarToken = null;
+			for(int i = 0; i < tokens.size(); i ++){
+				nameGrammarToken = tokens.get(i);
+				if(!nameGrammarToken.varibletoken()) {
+					writer.write(nameGrammarToken.getText());
+				}
+				else{
+//					Object va = classInfo.getPropertyValue(bean,nameGrammarToken.getFieldName());
+					Object va = getVariableValue.getValue(nameGrammarToken.getFieldName());
+					if(va == null)
+						throw new NameParserException(new StringBuilder()
+								.append(this.toString())
+								.append(",property[")
+								.append(nameGrammarToken.getFieldName()).append("] is null.").toString());
+					writer.write(String.valueOf(va));
+				}
+			}
 
+
+		}
+
+		/**
+		 * ClassUtil.ClassInfo classInfo, Object bean
+		 * @param builder
+		 * @param getVariableValue
+		 */
+		private void buildType(StringBuilder builder,GetVariableValue getVariableValue){
+			if(type != null){
+				builder.append(type);
+				return;
+			}
+			if(tokens == null || tokens.size() == 0){
+				return;
+			}
 			NameGrammarToken nameGrammarToken = null;
 			for(int i = 0; i < tokens.size(); i ++){
 				nameGrammarToken = tokens.get(i);
@@ -125,7 +259,8 @@ public class ESIndexWrapper {
 					builder.append(nameGrammarToken.getText());
 				}
 				else{
-					Object va = classInfo.getPropertyValue(bean,nameGrammarToken.getFieldName());
+//					Object va = classInfo.getPropertyValue(bean,nameGrammarToken.getFieldName());
+					Object va = getVariableValue.getValue(nameGrammarToken.getFieldName());
 					if(va == null)
 						throw new NameParserException(new StringBuilder()
 								.append(this.toString())
@@ -137,7 +272,13 @@ public class ESIndexWrapper {
 
 
 		}
-		public String buildType(ClassUtil.ClassInfo classInfo, Object bean){
+
+		/**
+		 * ClassUtil.ClassInfo classInfo, Object bean
+		 * @param getVariableValue
+		 * @return
+		 */
+		public String buildType(GetVariableValue getVariableValue){
 			if(type == null || type.equals("")){
 				if(tokens == null  || tokens.size() == 0 )
 					return null;
@@ -146,9 +287,36 @@ public class ESIndexWrapper {
 				return type;
 			}
 			StringBuilder builder = new StringBuilder();
-			buildType(builder,classInfo,bean);
+			buildType(builder,getVariableValue);
 			return builder.toString();
 		}
+
+	}
+	private static Map<String,String> parserVarinfo(String var){
+//		int idx = var.indexOf(",");
+		int idx = var.indexOf(",");
+		Map<String,String> varinfo = new HashMap();
+		if(idx > 0) {
+			String[] arr = var.split(",");
+
+			for (int i = 0; i < arr.length; i++) {
+				String e = arr[i];
+				String[] attr = e.split("=");
+				if (attr.length == 1)
+					throw new NameParserException(new StringBuilder()
+							.append("varible syntax error:")
+							.append(var)
+							.append(",")
+							.append(e)
+							.append(" must be a express like xxx=vvvv").toString());
+				varinfo.put(attr[0], attr[1]);
+			}
+
+		}
+		else{
+			varinfo.put("field",var);
+		}
+		return varinfo;
 
 	}
 	public static class NameGrammarToken extends TextGrammarParser.GrammarToken{
@@ -159,14 +327,15 @@ public class ESIndexWrapper {
 		public void after() {
 			try {
 				if (this.varibletoken()) {
-					int idx = text.indexOf(",");
-					if (idx > 0) {
-						fieldName = text.substring(0, idx);
-						dateformat = text.substring(idx + 1);
+
+					Map<String, String> varinfo = parserVarinfo(text);
+					fieldName = varinfo.get("field");
+					dateformat = varinfo.get("dateformat");
+					if (dateformat != null) {
 						dateFormateMeta = DateFormateMeta.buildDateFormateMeta(dateformat);
-					} else {
-						fieldName = text;
 					}
+
+
 				}
 			}
 			catch (Exception e){
@@ -191,17 +360,27 @@ public class ESIndexWrapper {
 
 	private NameInfo nameInfo;
 	private TypeInfo typeInfo;
+	private String index;
+	private String type;
 	private ESIndex esIndex;
 	public ESIndexWrapper(ESIndex esIndex){
 		this.esIndex = esIndex;
-		initInfo();
+		initInfo(esIndex.name(),esIndex.type());
 
 
 	}
-	private void initInfo(){
+	public ESIndexWrapper(String indexPattern,String typePattern){
+
+		initInfo(indexPattern,typePattern);
+
+
+	}
+	private void initInfo(String name,String type){
+		this.index = name;
+		this.type = type;
 		nameInfo = new NameInfo();
-		String name = esIndex.name();
-		List<NameGrammarToken> tokens = TextGrammarParser.parser(name, '{', '}',nameGrammarTokenBuilder);
+//		String name = esIndex.name();
+		List<NameGrammarToken> tokens = TextGrammarParser.parser(index, '{', '}',nameGrammarTokenBuilder);
 		boolean varibled = false;
 		for(int i = 0; tokens != null && i < tokens.size(); i ++){
 			TextGrammarParser.GrammarToken token = tokens.get(i);
@@ -214,11 +393,11 @@ public class ESIndexWrapper {
 			nameInfo.tokens = tokens;
 		}
 		else{
-			nameInfo.name = name;
+			nameInfo.name = index;
 		}
-		if (esIndex.type() != null) {
+		if (type != null) {
 			typeInfo = new TypeInfo();
-			String type = esIndex.type();
+//			String type = esIndex.type();
 			tokens = TextGrammarParser.parser(type, '{', '}', nameGrammarTokenBuilder);
 			varibled = false;
 			for (int i = 0; tokens != null && i < tokens.size(); i++) {
@@ -235,18 +414,28 @@ public class ESIndexWrapper {
 			}
 		}
 	}
-	public void buildIndexName(StringBuilder builder,ClassUtil.ClassInfo classInfo,Object bean){
-		  nameInfo.buildName(builder,classInfo,bean);
+	public void buildIndexName(Writer writer, GetVariableValue getVariableValue) throws IOException {
+		nameInfo.buildName(writer,  getVariableValue);
+	}
+	public void buildIndexName(StringBuilder builder,GetVariableValue getVariableValue){
+		  nameInfo.buildName(builder,  getVariableValue);
 	}
 
-	public String buildIndexName(ClassUtil.ClassInfo classInfo,Object bean){
-		return nameInfo.buildName(classInfo,bean);
+	public String buildIndexName(GetVariableValue getVariableValue){
+		return nameInfo.buildName(  getVariableValue);
 	}
-	public void buildIndexType(StringBuilder builder,ClassUtil.ClassInfo classInfo,Object bean){
-		typeInfo.buildType(builder,classInfo,bean);
+	public void buildIndexType(Writer writer,GetVariableValue getVariableValue) throws IOException {
+		typeInfo.buildType(  writer,  getVariableValue);
+	}
+	public void buildIndexType(StringBuilder builder,GetVariableValue getVariableValue){
+		typeInfo.buildType(builder,  getVariableValue);
 	}
 
-	public String buildIndexType(ClassUtil.ClassInfo classInfo,Object bean){
-		return typeInfo.buildType(classInfo,bean);
+	public String buildIndexType(GetVariableValue getVariableValue){
+		return typeInfo.buildType(  getVariableValue);
+	}
+
+	public TypeInfo getTypeInfo(){
+		return typeInfo;
 	}
 }
