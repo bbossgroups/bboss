02c2fc697fe7559b8c9bb8db7795dd6cf4a1795e
yinbp <yin-bp@163.com>
2019-11-18 00:56:39 +0800

    数据同步工具改进：mongodb-es结构调整 数据同步工具扩展：增加mongodb-db同步功能，开发中....

--- bboss-core-entity/src/com/frameworkset/util/ValueCastUtil.java
+++ bboss-core-entity/src/com/frameworkset/util/ValueCastUtil.java
@@ -15,6 +15,8 @@ package com.frameworkset.util;
  * limitations under the License.
  */
 
+import java.math.BigDecimal;
+
 /**
  * <p>Description: </p>
  * <p></p>
@@ -84,5 +86,149 @@ public abstract class ValueCastUtil {
 			return (String)value;
 		}
 		return String.valueOf(value);
+	}
+
+
+	public static  Integer integerValue(Object num,Integer defaultValue){
+		return intValue(num,defaultValue);
+	}
+	public static  Long longValue(Object num,Long defaultValue){
+		if(num == null)
+			return defaultValue;
+		if(num instanceof Long)
+		{
+			return ((Long)num);
+		}
+		else  if(num instanceof BigDecimal)
+		{
+			return ((BigDecimal)num).longValue();
+		}
+		else if(num instanceof Double)
+		{
+			return ((Double)num).longValue();
+		}else if(num instanceof Integer){
+			return ((Integer)num).longValue();
+		}
+		else if(num instanceof Float)
+		{
+			return ((Float)num).longValue();
+		}
+		else  if(num instanceof Short)
+		{
+			return ((Short)num).longValue();
+		}
+		else
+		{
+			return Long.parseLong(num.toString());
+		}
+	}
+
+	public static  Integer intValue(Object num,Integer defaultValue){
+		if(num == null)
+			return defaultValue;
+		if(num instanceof Integer)
+		{
+			return ((Integer)num);
+		}
+		else if(num instanceof Long)
+		{
+			return ((Long)num).intValue();
+		}
+		else  if(num instanceof BigDecimal)
+		{
+			return ((BigDecimal)num).intValue();
+		}
+		else if(num instanceof Double)
+		{
+			return ((Double)num).intValue();
+		}
+		else if(num instanceof Float)
+		{
+			return ((Float)num).intValue();
+		}
+		else  if(num instanceof Short)
+		{
+			return ((Short)num).intValue();
+		}
+		else
+		{
+			return Integer.parseInt(num.toString());
+		}
+	}
+
+	public static  Float floatValue(Object num,Float defaultValue){
+		if(num == null)
+			return defaultValue;
+		if(num instanceof Float)
+		{
+			return (Float)num;
+		}
+		else  if(num instanceof BigDecimal)
+		{
+			return ((BigDecimal)num).floatValue();
+		}
+		else if(num instanceof Double)
+		{
+			return ((Double)num).floatValue();
+		}else if(num instanceof Integer){
+			return ((Integer)num).floatValue();
+		}
+		else  if(num instanceof Long)
+		{
+			return ((Long)num).floatValue();
+		}
+		else  if(num instanceof Short)
+		{
+			return ((Short)num).floatValue();
+		}
+		else
+		{
+			return Float.parseFloat(num.toString());
+		}
+	}
+
+	public static  String stringValue(Object num,String defaultValue){
+		if(num == null)
+			return defaultValue;
+		if(num instanceof String)
+		{
+			return (String)num;
+		}
+		else
+		{
+			return String.valueOf(num);
+		}
+
+	}
+	public static  Double doubleValue(Object num,Double defaultValue){
+		if(num == null)
+			return defaultValue;
+		if(num instanceof Double)
+		{
+			return (Double)num;
+		}
+		else  if(num instanceof BigDecimal)
+		{
+			return ((BigDecimal)num).doubleValue();
+		}
+		else if(num instanceof Float)
+		{
+			return ((Float)num).doubleValue();
+		}else if(num instanceof Integer){
+			return ((Integer)num).doubleValue();
+		}
+		else  if(num instanceof Long)
+		{
+			return ((Long)num).doubleValue();
+		}
+		else  if(num instanceof Short)
+		{
+			return ((Short)num).doubleValue();
+		}
+		else
+		{
+
+			return Double.parseDouble(num.toString());
+		}
 	}
 }
