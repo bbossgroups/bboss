a9b846e4c8dd7f726a2051e10a1355442b668a9f
大河 <yin-bp@163.com>
2023-10-24 00:11:44 +0800

    修复Elasticsearch Client failAllContinue配置为true时，不起作用问题

--- bboss-util/test/org/frameworkset/util/TextGrammarParserTest.java
+++ bboss-util/test/org/frameworkset/util/TextGrammarParserTest.java
@@ -1,10 +1,13 @@
 package org.frameworkset.util;
 
+import com.frameworkset.util.RegexUtil;
 import org.frameworkset.util.tokenizer.TextGrammarParser;
 import org.frameworkset.util.tokenizer.TextGrammarParser.GrammarToken;
 import org.junit.Test;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TextGrammarParserTest {
 
@@ -75,6 +78,39 @@ public class TextGrammarParserTest {
 		tokens = TextGrammarParser.parser(content, tokenpre, tokenend);
 		System.out.println(content+"分词结果：\r\n");
 		System.out.println(tokens);
+	}
+
+	@Test
+	public void varialValue(){
+		String tokenpre = "${";
+
+		String tokenend = "}";
+		String str1 = "${a} hello ${b}";
+		List<GrammarToken> tokens = TextGrammarParser.parser(str1, tokenpre, tokenend);
+		StringBuilder pattern = new StringBuilder();
+		for(GrammarToken token :tokens){
+			if(token.texttoken()){
+				pattern.append(token.getText());
+			}
+			else{
+				pattern.append("(").append(".*").append(")");
+			}
+		}
+
+
+		String str2 = "666 c  hello 777";
+		String[] values = RegexUtil.parser(str2,pattern.toString());
+		Map<String,String> data = new LinkedHashMap<>();
+		int i = 0;
+		for(GrammarToken token :tokens){
+			if(token.varibletoken()){
+				data.put(token.getText(),values[i]);
+				i ++;
+			}
+		}
+		System.out.println(data);
+
+
 	}
 
 }
